845de2f20fb93060c9484671f6956f6d918d7bd9
Akshay S <akshaysclt2002@gmail.com>
2024-11-29 09:41:35 +0530

    minor update in script.js

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -27,8 +27,13 @@ const trainee = {
 }
 console.log("trainee has the datatype:" + typeof trainee);
 
+const sample_array =["Akshay",22,30.15,false,null,undefined,NaN];
+sample_array.forEach(value => {
+    console.log(value,typeof value);
+});
+
 //2.Write a JS program to show an alert message on the loading of the website.
-alert("The Website is Loading.Please Wait")
+alert("The Website is Loading.Please Wait");
 
 //3a. Remove number "6" from the array and console the length of the array.
 const array = ["1", "2", "3", "4", "5", "6", "7"];
@@ -38,11 +43,10 @@ console.log("length of array is : " + array.length);
 
 //3b.Convert all the items of the array to data type number and console each items data type, use any of the array iteration methods provided by JS for iteration.
 const arr = ["1", "2", "3", "4", "5", "6", "7"];
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = parseInt(arr[i]);
-    console.log(typeof arr[i]);
-}
-console.log(arr);
+arr.forEach((element) => {
+    element=parseInt(element);
+    console.log(typeof element);
+});
 
 //3c.Remove last three items of the array, use JS provided array method, then console the array and then add "one" and "two" (strings) to the beginning of the array and console the array. 
 const array3 = ["1", "2", "3", "4", "5", "6", "7"];
@@ -54,16 +58,16 @@ console.log(array3);
 //3d.Using any one of the array iteration methods console the string concatenation of all items of the array and also console the sum of all the items ( convert to integer before calculating)
 const array4 = ["1", "2", "3", "4", "5", "6", "7"];
 var result = "";
-for (let i = 0; i < array4.length; i++) {
-    result += array4[i];
-}
+array4.forEach((element) => {
+    result+=element;
+});
 console.log(result);
 
 var sum = 0;
-for (let i = 0; i < array4.length; i++) {
-    array4[i] = parseInt(array4[i]);
-    sum += array4[i];
-}
+array4.forEach((element) => {
+    element=parseInt(element);
+    sum+=element;
+});
 console.log(sum);
 
 //3e.Filter out item "3" from the array and console the array (use array method)
@@ -73,11 +77,11 @@ console.log(array5);
 
 //3f.Iterate the array and console the item, when item is either "3", "6" or "7"
 let array6 = ["1", "2", "3", "4", "5", "6", "7"];
-for (let i = 0; i < array6.length; i++) {
-    if (array6[i] == 3 || array6[i] == 6 || array6[i] == 7) {
-        console.log(array6[i]);
+array6.forEach((element) => {
+    if(element==3 || element==6 || element==7){
+        console.log(element);
     }
-}
+});
 
 //3g.[1, 2, "3", 4, 5, 6,"7"]  Compare this array with the above given array and console only if both items of the array have same data type. (Compare each item of this array with each item of the other array)
 let array_string = ["1", "2", "3", "4", "5", "6", "7"];
